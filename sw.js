8b0996107152e841d0c80aec152d350c67bf637b
Amber Suratte <as00193@my.westga.edu>
2022-07-15 15:37:49 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d35ebf70e2234444d220.js"
+    "url": "webpack-runtime-b8030960834a51ba7157.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "be41d07bee508e61d1d05dd71d49c54a"
+    "revision": "11117c4c5da8e4815e428170fdde01cc"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9e5ad13884b9c23a3dc.js"
